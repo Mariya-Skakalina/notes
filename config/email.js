af748b67368f969d8b9688d90784c17142b9c9e3
Maria <scka.maria@gmail.com>
2019-01-03 01:57:11 +0300

    em

--- config/email.js
+++ config/email.js
@@ -1,9 +1,14 @@
 'use strict';
 const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
+    user:    "user", 
+    password: "password", 
     host: 'localhost',
     port: 587,
     secure: false, // upgrade later with STARTTLS
+    tls:{
+        rejectUnauthorized: false
+    }
 });
 
 module.exports.transporter = transporter
