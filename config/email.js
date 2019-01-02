4a3aa830733a093a161a94c93d78b0b059b4e1b7
Maria <scka.maria@gmail.com>
2019-01-03 01:30:57 +0300

    postfix

--- config/email.js
+++ config/email.js
@@ -1,13 +1,9 @@
 'use strict';
 const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
-    host: 'smtp.example.com',
+    host: 'localhost',
     port: 587,
     secure: false, // upgrade later with STARTTLS
-    auth: {
-        user: "admin", // generated ethereal user
-        pass: "12345" // generated ethereal password
-    }
 });
 
 module.exports.transporter = transporter
